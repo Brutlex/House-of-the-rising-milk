b0747adb5ab62ebfb99a35e8b10a7015340cff01
DemiMarieD <demi-marie@outlook.de>
2017-04-28 22:43:25 +0200

    Test: Physik cookie in Objekt umgewandelt

--- main.lua
+++ main.lua
@@ -4,6 +4,17 @@ function love.load()
   --test
   cloud1 = love.graphics.newImage("gfx/cloud1.png")
   cookie1 = love.graphics.newImage("gfx/Cookie1.1.png")
+  
+  love.physics.setMeter(64)
+  world = love.physics.newWorld(0, 12*64, true)
+  
+  objects = {}
+  
+  objects.cookie = {}
+  objects.cookie.body =  love.physics.newBody(world, 1024/2, 768/2, "dynamic")
+  objects.cookie.shape = love.physics.newCircleShape(40)
+  objects.cookie.fixture = love.physics.newFixture(objects.cookie.body, objects.cookie.shape, 1)
+
   -- comment
   love.graphics.setBackgroundColor(177, 215, 231)
 end
@@ -14,7 +25,7 @@ end
  
 
 function love.draw()
-  love.graphics.draw(cookie1, 100, 100)
+  objects.cookie.fixture:setRestitution(0.9)love.graphics.draw(cookie1, objects.cookie.body:getX(), objects.cookie.body:getY())
   love.graphics.draw(cloud1, 300, 300)
   love.graphics.rectangle("fill", 0, love.graphics.getHeight() - 100, love.graphics.getWidth(), 100)
   
