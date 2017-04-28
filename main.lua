046049f511c0abf97e5b533b7e86b3e92e16002f
Stephanie <stephanie.kuehnl@gmail.com>
2017-04-28 21:48:36 +0200

    Cloud added

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 function love.load()
-  
+  cloud1 = love.graphics.newImage("cloud1.png")
   cookie1 = love.graphics.newImage("Cookie1.1.png")
 end
  
@@ -11,5 +11,7 @@ end
 function love.draw()
   love.graphics.setBackgroundColor(177, 215, 231)
   love.graphics.draw(cookie1, 100, 100)
+  love.graphics.draw(cloud1, 300, 300)
   love.graphics.rectangle("fill", 0, love.graphics.getHeight() - 100, love.graphics.getWidth(), 100)
+  
 end
