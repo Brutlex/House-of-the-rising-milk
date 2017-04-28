d12f759daed4c5929024bd6a382c651cffe6c370
hollunder <murks@tuxfamily.org>
2017-04-28 21:58:24 +0200

    set background color in load

--- main.lua
+++ main.lua
@@ -3,6 +3,8 @@ local anim8 = require('lib/anim8/anim8')
 function love.load()
   cloud1 = love.graphics.newImage("gfx/cloud1.png")
   cookie1 = love.graphics.newImage("gfx/Cookie1.1.png")
+  
+  love.graphics.setBackgroundColor(177, 215, 231)
 end
  
 
@@ -11,7 +13,6 @@ end
  
 
 function love.draw()
-  love.graphics.setBackgroundColor(177, 215, 231)
   love.graphics.draw(cookie1, 100, 100)
   love.graphics.draw(cloud1, 300, 300)
   love.graphics.rectangle("fill", 0, love.graphics.getHeight() - 100, love.graphics.getWidth(), 100)
