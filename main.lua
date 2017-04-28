73aeced6ba4811be006c00771c463837fc12314f
hollunder <murks@tuxfamily.org>
2017-04-28 21:35:09 +0200

    background color

--- main.lua
+++ main.lua
@@ -1,14 +1,15 @@
 function love.load()
+  
   cookie1 = love.graphics.newImage("Cookie1.1.png")
 end
  
 
 function love.update(dt)
-print(love.graphics.getHeight())
 end
  
 
 function love.draw()
+  love.graphics.setBackgroundColor(177, 215, 231)
   love.graphics.draw(cookie1, 100, 100)
   love.graphics.rectangle("fill", 0, love.graphics.getHeight() - 100, love.graphics.getWidth(), 100)
 end
