827214ceabc3365e6cec3eae38ccc4b08e16fd07
hollunder <murks@tuxfamily.org>
2017-04-29 21:55:28 +0200

    canvas*3

--- scrollcanvas.lua
+++ scrollcanvas.lua
@@ -4,25 +4,26 @@ local class = require('lib/middleclass')
 SC = class('SC')
 
 function SC:initialize()
-  self.dc = C.H -- offset between canvases
+  
+  self.dc = C.H*3 -- offset between canvases
   self.speed = 50
 
-  self.cb = love.graphics.newCanvas(C.W, C.H)
-  self.cby = -C.H -- start completely outside the screen
+  self.cb = love.graphics.newCanvas(C.W, self.dc)
+  self.cby = -(5*C.H) -- start completely outside the screen
 
-  self.ca = love.graphics.newCanvas(C.W, C.H)
+  self.ca = love.graphics.newCanvas(C.W, self.dc)
   self.cay = self.cby + self.dc -- start at screen position
 
 --[[
   love.graphics.setCanvas(self.cb)
   love.graphics.clear()
   love.graphics.setColor(100, 100, 200)
-  love.graphics.rectangle("line", 0, 0, C.W, C.H)
+  love.graphics.rectangle("fill", 0, 0, C.W, self.dc)
   
   love.graphics.setCanvas(self.ca)
   love.graphics.clear()
   love.graphics.setColor(200, 100, 100)
-  love.graphics.rectangle("line", 0, 0, C.W, C.H)
+  love.graphics.rectangle("fill", 0, 0, C.W, self.dc)
   
   love.graphics.setCanvas()
   love.graphics.setBlendMode("alpha")
@@ -36,8 +37,8 @@ function SC:update(dt)
   local tcby = self.cby + offset
   local tcay = tcby + self.dc
 
-  if tcay >= C.H-1 then tcay = tcay - 2*C.H end
-  if tcby >= C.H-1 then tcby = tcby - 2*C.H end
+  if tcay >= self.dc-1 then tcay = tcay - 2*self.dc end
+  if tcby >= self.dc-1 then tcby = tcby - 2*self.dc end
 
   self.cay = tcay
   self.cby = tcby
